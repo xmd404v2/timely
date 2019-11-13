760e76655bc8effdd4820f5a4ff528aa6f0511f8
Xavier Duncan <xduncan@pm.me>
2019-11-12 23:58:36 -0600

    add more quotes :)

--- js/scripts.js
+++ js/scripts.js
@@ -29,8 +29,16 @@ const quotes = [
   '"Time is the wisest counselor of all." – Pericles.',
   '"The key is in not spending time, but in investing it." – Stephen R. Covey.',
   '"It is the time you have wasted for your rose that makes your rose so important." – Antoine de Saint-Exupéry, The Little Prince.',
-  '"Punctuality is the thief of time." – Oscar Wilde, The Picture of Dorian Gray.' ];
+  '"Punctuality is the thief of time." – Oscar Wilde, The Picture of Dorian Gray.',
+  '"you can never get it back." – Harvey Mackay',
+  '"The trouble is, you think you have time." – Jack Kornfield',
+  '"Time is a cruel thief to rob us of our former selves. We lose as much to life as we do to death." - Elizabeth Forsythe Hailey',
+  '"Men talk of killing time, while time quietly kills them." - Dion Boucicault',
+  '"There is one kind of robber whom the law does not strike at, and who steals what is most precious to men: time." - Napoleon I',
+  '"You can’t have a better tomorrow if you are thinking about yesterday all the time." - Charles F. Kettering',
+  '"Time spent with cats is never wasted." - Colette',
+  '"It’s not that we have little time, but more that we waste a good deal of it." - Seneca', ];
 (function randomQuote() {
-	let randomNumber = Math.floor(Math.random() * quotes.length);
+  let randomNumber = Math.floor(Math.random() * quotes.length);
 	document.getElementById('quote').innerHTML = quotes[randomNumber];
 })();
