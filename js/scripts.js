e3b6960263b989344accd33f4f9e3710bab06b79
Xavier Duncan <xduncan@pm.me>
2019-11-13 07:53:08 -0600

    format quotes

--- js/scripts.js
+++ js/scripts.js
@@ -30,14 +30,14 @@ const quotes = [
   '"The key is in not spending time, but in investing it." – Stephen R. Covey.',
   '"It is the time you have wasted for your rose that makes your rose so important." – Antoine de Saint-Exupéry, The Little Prince.',
   '"Punctuality is the thief of time." – Oscar Wilde, The Picture of Dorian Gray.',
-  '"you can never get it back." – Harvey Mackay',
-  '"The trouble is, you think you have time." – Jack Kornfield',
-  '"Time is a cruel thief to rob us of our former selves. We lose as much to life as we do to death." - Elizabeth Forsythe Hailey',
-  '"Men talk of killing time, while time quietly kills them." - Dion Boucicault',
-  '"There is one kind of robber whom the law does not strike at, and who steals what is most precious to men: time." - Napoleon I',
-  '"You can’t have a better tomorrow if you are thinking about yesterday all the time." - Charles F. Kettering',
-  '"Time spent with cats is never wasted." - Colette',
-  '"It’s not that we have little time, but more that we waste a good deal of it." - Seneca', ];
+  '"you can never get it back." – Harvey Mackay.',
+  '"The trouble is, you think you have time." – Jack Kornfield.',
+  '"Time is a cruel thief to rob us of our former selves. We lose as much to life as we do to death." - Elizabeth Forsythe Hailey.',
+  '"Men talk of killing time, while time quietly kills them." - Dion Boucicault.',
+  '"There is one kind of robber whom the law does not strike at, and who steals what is most precious to men: time." - Napoleon I.',
+  '"You can’t have a better tomorrow if you are thinking about yesterday all the time." - Charles F. Kettering.',
+  '"Time spent with cats is never wasted." - Colette.',
+  '"It’s not that we have little time, but more that we waste a good deal of it." - Seneca.', ];
 (function randomQuote() {
   let randomNumber = Math.floor(Math.random() * quotes.length);
 	document.getElementById('quote').innerHTML = quotes[randomNumber];
