78b6e08fba646555507aa21d6c9158e386016133
Xavier Duncan <xduncan@pm.me>
2019-11-13 19:21:07 -0600

    fix timer style bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,3 @@
-const quotes = [
-	'"The two most powerful warriors are patience and time." – Leo Tolstoy, War and Peace.',
-	'"Time is money." – Benjamin Franklin.',
-	'"Time waits for no one." – Folklore.',
-	'"Better three hours too soon than a minute too late." – William Shakespeare.',
-  '"Lost time is never found again." – Benjamin Franklin.',
-  '"Time is the most valuable thing a man can spend." – Theophrastus.',
-  '"Time is the wisest counselor of all." – Pericles.',
-  '"The key is in not spending time, but in investing it." – Stephen R. Covey.',
-  '"It is the time you have wasted for your rose that makes your rose so important." – Antoine de Saint-Exupéry, The Little Prince.',
-  '"Punctuality is the thief of time." – Oscar Wilde, The Picture of Dorian Gray.',
-  '"The trouble is, you think you have time." – Jack Kornfield.',
-  '"Time is a cruel thief to rob us of our former selves. We lose as much to life as we do to death." - Elizabeth Forsythe Hailey.',
-  '"Men talk of killing time, while time quietly kills them." - Dion Boucicault.',
-  '"There is one kind of robber whom the law does not strike at, and who steals what is most precious to men: time." - Napoleon I.',
-  '"You can’t have a better tomorrow if you are thinking about yesterday all the time." - Charles F. Kettering.',
-	'"It’s not that we have little time, but more that we waste a good deal of it." - Seneca.', ];
-	
-(function randomQuote() {
-  const randomNumber = Math.floor(Math.random() * quotes.length);
-	document.getElementById('quote').innerHTML = quotes[randomNumber];
-})();
-
 const countDownDate = new Date('Dec 6, 2099 00:00:00').getTime();
 
 const x = setInterval(function() {
@@ -40,3 +17,27 @@ const x = setInterval(function() {
 	}
 	// Update the count down every 1 second
 }, 1000);
+
+const quotes = [
+	'"The two most powerful warriors are patience and time." – Leo Tolstoy, War and Peace.',
+	'"Time is money." – Benjamin Franklin.',
+	'"Time waits for no one." – Folklore.',
+	'"Better three hours too soon than a minute too late." – William Shakespeare.',
+	'"Lost time is never found again." – Benjamin Franklin.',
+	'"Time is the most valuable thing a man can spend." – Theophrastus.',
+	'"Time is the wisest counselor of all." – Pericles.',
+	'"The key is in not spending time, but in investing it." – Stephen R. Covey.',
+	'"It is the time you have wasted for your rose that makes your rose so important." – Antoine de Saint-Exupéry, The Little Prince.',
+	'"Punctuality is the thief of time." – Oscar Wilde, The Picture of Dorian Gray.',
+	'"The trouble is, you think you have time." – Jack Kornfield.',
+	'"Time is a cruel thief to rob us of our former selves. We lose as much to life as we do to death." - Elizabeth Forsythe Hailey.',
+	'"Men talk of killing time, while time quietly kills them." - Dion Boucicault.',
+	'"There is one kind of robber whom the law does not strike at, and who steals what is most precious to men: time." - Napoleon I.',
+	'"You can’t have a better tomorrow if you are thinking about yesterday all the time." - Charles F. Kettering.',
+	'"It’s not that we have little time, but more that we waste a good deal of it." - Seneca.'
+];
+
+(function randomQuote() {
+	const randomNumber = Math.floor(Math.random() * quotes.length);
+	document.getElementById('quote').innerHTML = quotes[randomNumber];
+})();
