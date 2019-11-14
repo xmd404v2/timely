c574facda1828c53b9cb2deda7fae64ffb8fbe48
Xavier Duncan <xduncan@pm.me>
2019-11-13 19:13:58 -0600

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,3 @@
-// Set the date we're counting down to
-const countDownDate = new Date('Dec 6, 2099 00:00:00').getTime();
-// Update the count down every 1 second
-const x = setInterval(function() {
-	// Get todays date and time
-	const now = new Date().getTime();
-	// Find the distance between now an the count down date
-	const distance = countDownDate - now;
-	// Time calculations for days, hours, minutes and seconds
-	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	// Output the result in an element with id="demo"
-	document.getElementById('demo').innerHTML = hours + 'h ' + minutes + 'm ' + seconds + 's ';
-	// If the count down is over, write some text
-	if (distance < 0) {
-		clearInterval(x);
-		document.getElementById('demo').innerHTML = 'EXPIRED';
-	}
-}, 1000);
-// randomQuote generator
 const quotes = [
 	'"The two most powerful warriors are patience and time." – Leo Tolstoy, War and Peace.',
 	'"Time is money." – Benjamin Franklin.',
@@ -35,8 +14,29 @@ const quotes = [
   '"Men talk of killing time, while time quietly kills them." - Dion Boucicault.',
   '"There is one kind of robber whom the law does not strike at, and who steals what is most precious to men: time." - Napoleon I.',
   '"You can’t have a better tomorrow if you are thinking about yesterday all the time." - Charles F. Kettering.',
-  '"It’s not that we have little time, but more that we waste a good deal of it." - Seneca.', ];
+	'"It’s not that we have little time, but more that we waste a good deal of it." - Seneca.', ];
+	
 (function randomQuote() {
   const randomNumber = Math.floor(Math.random() * quotes.length);
 	document.getElementById('quote').innerHTML = quotes[randomNumber];
 })();
+
+const countDownDate = new Date('Dec 6, 2099 00:00:00').getTime();
+
+const x = setInterval(function() {
+	// Get todays date and time
+	const now = new Date().getTime();
+	// Find the distance between now an the count down date
+	const distance = countDownDate - now;
+	// Time calculations for days, hours, minutes and seconds
+	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+	document.getElementById('timer').innerHTML = `${hours}h ${minutes}m ${seconds}s`;
+	// If the count down is over, write some text
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById('timer').innerHTML = 'EXPIRED';
+	}
+	// Update the count down every 1 second
+}, 1000);
